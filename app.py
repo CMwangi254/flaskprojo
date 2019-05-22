13aae66c57eaeb4d38dd08b18729205bd3643c72
malenge254 <techiechic12.34@gmail.com>
2019-05-22 07:24:25 +0000

    app file update

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-from datetime import datetime
+from flask import Flask
 
-from flask import Flask, render_template
+import views
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home_page():
-    today = datetime.today()
-    day_name = today.strftime("%A")
-    return render_template("home.html", day=day_name)
+def create_app():
+    app.add_url_rule("/", "home_page", view_func=views.home_page)
+    app.add_url_rule("/movies", "movies_page", view_func=views.movies_page)
 
-@app.route('/movies')
-def movies_page():
-    return render_template("movielist.html")
+
+create_app()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run()
