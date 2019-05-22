85a0d6b42b37ca620a0c3360b29ea5c9b89409fa
malenge254 <techiechic12.34@gmail.com>
2019-05-22 07:42:47 +0000

    app file second update

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import views
 app = Flask(__name__)
 
 
-def create_app():
-    app.add_url_rule("/", "home_page", view_func=views.home_page)
-    app.add_url_rule("/movies", "movies_page", view_func=views.movies_page)
+# def create_app():
+app.add_url_rule("/", "home_page", view_func=views.home_page)
+app.add_url_rule("/movies", "movies_page", view_func=views.movies_page)
 
 
-create_app()
+# create_app()
 
 
 if __name__ == '__main__':
