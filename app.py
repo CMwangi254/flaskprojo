69d832075d5a6704cea3182a14360d5be7b91183
malenge254 <techiechic12.34@gmail.com>
2019-05-23 13:05:08 +0000

    home update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 import views
+from database import Database
+from movie import Movie
 
 app = Flask(__name__)
 
@@ -8,6 +10,12 @@ app = Flask(__name__)
 # def create_app():
 app.add_url_rule("/", "home_page", view_func=views.home_page)
 app.add_url_rule("/movies", "movies_page", view_func=views.movies_page)
+app.add_url_rule("/movies/<int:movie_key>", "movie_page", view_func=views.movie_page)
+
+db = Database()
+db.add_movie(Movie("The SlaughterHouse", year=1972))
+db.add_movie(Movie("The Shining"))
+app.config["db"] = db
 
 
 # create_app()
