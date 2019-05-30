c6b1e94489ce19a2bf48d2e3b8af4be76a9e2117
malenge254 <techiechic12.34@gmail.com>
2019-05-30 00:03:49 +0000

    home update

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ from flask import Flask
 
 import views
 from database import Database
-from movie import Movie
 
 app = Flask(__name__)
 
 
 # def create_app():
 app.add_url_rule("/", "home_page", view_func=views.home_page)
-app.add_url_rule("/movies", "movies_page", view_func=views.movies_page)
+app.add_url_rule("/movies", "movies_page", view_func=views.movies_page, methods=["GET", "POST"])
 app.add_url_rule("/movies/<int:movie_key>", "movie_page", view_func=views.movie_page)
+app.add_url_rule("/new-movie", "movie_add_page", view_func=views.movie_add_page, methods=["GET", "POST"])
+app.add_url_rule("/movies/<int:movie_key>/edit", "movie_edit_page", view_func=views.movie_edit_page, methods=["GET", "POST"])
 
 db = Database()
-db.add_movie(Movie("The SlaughterHouse", year=1972))
-db.add_movie(Movie("The Shining"))
 app.config["db"] = db
 
 
--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from flask import abort, current_app, render_template
+from flask import abort, current_app, render_template, request, redirect, url_for
+
+from movie import Movie
 
 
 def home_page():
@@ -11,8 +13,14 @@ def home_page():
 
 def movies_page():
     db = current_app.config["db"]
-    movies = db.get_movies()
-    return render_template("movielist.html", movies=sorted(movies))
+    if request.method == 'GET':
+        movies = db.get_movies()
+        return render_template("movielist.html", movies=sorted(movies))
+    else:
+        form_movie_keys = request.form.getlist("movie_keys")
+        for form_movie_key in form_movie_keys:
+            db.delete_movie(int(form_movie_key))
+        return redirect(url_for("movies_page"))
 
 
 def movie_page(movie_key):
@@ -21,3 +29,33 @@ def movie_page(movie_key):
     if movie is None:
         abort(404)
     return render_template("movie.html", movie=movie)
+
+
+def movie_add_page():
+    if request.method == "GET":
+        values = {"title": "", "year": ""}
+        return render_template("movie_edit.html", min_year=1887, max_year=datetime.now().year,  values=values,)
+    else:
+        form_title = request.form.get("title")
+        form_year = request.form.get("year")
+        movie = Movie(form_title, year=int(form_year) if form_year else None)
+        db = current_app.config["db"]
+        movie_key = db.add_movie(movie)
+        return redirect(url_for("movie_page", movie_key=movie_key))
+
+
+def movie_edit_page(movie_key):
+    if request.method == "GET":
+        db = current_app.config["db"]
+        movie = db.get_movie(movie_key)
+        if movie is None:
+            abort(404)
+        values = {"title": movie.title, "year": movie.year}
+        return render_template("movie_edit.html", min_year=1887, max_year=datetime.now().year, values=values,)
+    else:
+        form_title = request.form.get("title")
+        form_year = request.form.get("year")
+        movie = Movie(form_title, year=int(form_year) if form_year else None)
+        db = current_app.config["db"]
+        db.update_movie(movie_key, movie)
+        return redirect(url_for("movie_page", movie_key=movie_key))
